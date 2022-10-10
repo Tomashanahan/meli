9c8580bc8c926eb11e98be7c6f97bccac8e0cd6a
Tomas Shanahan <tshmg@hotmail.com>
2022-10-10 17:42:41 -0300

    Agregando una alerta phishing

--- src/App.js
+++ src/App.js
@@ -4,8 +4,32 @@ import Header from "./Components/Header/Header.jsx";
 import Detail from "./Components/Product Detail/Detail";
 import { Routes, Route } from "react-router-dom";
 import SearchedProduct from "./Components/SearchedProduct/SearchedProduct";
+import { useEffect } from "react";
+import swal from "sweetalert";
 
 function App() {
+	const phishingAletr = () => {
+		swal({
+			title: "Aviso Importante!",
+			text: "Esta no es una pagina oficial de Mercado Libre!!",
+			icon: "warning",
+			buttons: true,
+			dangerMode: true,
+		}).then((willDelete) => {
+			if (willDelete) {
+				swal("Consentimiento aceptado por el usuario.", {
+					icon: "success",
+				});
+			} else {
+				phishingAletr();
+			}
+		});
+	};
+
+	useEffect(() => {
+		phishingAletr();
+	}, []);
+
 	return (
 		<Box className="App" bg="#ededed" pos="relative" pb="200px">
 			<Header />
